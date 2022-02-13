2d32f9d5c0fa2502999bbedd25b02a46b342d41f
AvaniCoder <80157948+AvaniCoder@users.noreply.github.com>
2022-02-13 14:40:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+var mouseEvent = "Empty";
+
+color = "Red";
+width_of_line = 2;
+
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+
+canvas.addEventListener("mousedown", my_mousedown);
+
+function my_mousedown(e)
+{
+    color - document.getElementById("color").value;
+    width_of_line - document.getElementById("width_of_line").value;
+    radius - document.getElementById("radius").value;
+    mouseEvent - "mousedown";
+}
+
+canvas.addEventListener("mousemove", my_mousemove);
+
+function my_mousemove(e)
+{
+    current_position_of_mouse_x=e.clientX-canvas.offsetLeft;
+    current_position_of_mouse_y=e.clientX-canvas.offsetTop;
+
+    if(mouseEvent=="mousedown")
+    {
+    console.log("current position of x and y=");
+    console.log("x="+current_position_of_mouse_x+"y="+current_position_of_mouse_y);
+    ctx.beginPath();
+    ctx.strokeStyle-color;
+    ctx.lineWidth-width_of_line;
+    ctx.arc(current_position_of_mouse_x, current_position_of_mouse_y, radius, 0, 2*Math.PI);
+    ctx.stroke();
+    }
+}
